9fc0e8aa37f9ebf41ffba82bed489b91a1491a4a
FDVleuten <86839267+FDVleuten@users.noreply.github.com>
2021-08-17 16:07:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+const theAge = function (number) {
+  if (number => 18) {
+    return true 
+  } else {
+
+    return false
+  };
+}
+  const theAge2 = function (number) {
+    if (theAge => 18) {
+      return ("Hello there") 
+    } else {
+  
+      return ("bye Kiddo")
+    };
+  }
+
+  const result = theAge(18);
+  console.log(result);
+
